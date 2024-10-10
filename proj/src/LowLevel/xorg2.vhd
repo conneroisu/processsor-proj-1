0d0de2967a5c5abe34eb2769ab8fd83752bf5c99
conneroisu <conneroisu@outlook.com>
2024-10-10 10:19:21 -0400

    add lowlevel components and testbenches

---
+++ proj/src/LowLevel/xorg2.vhd
@@ -0,0 +1,19 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity xorg2 is
+
+  port (
+    i_A : in  std_logic;
+    i_B : in  std_logic;
+    o_F : out std_logic
+    );
+
+end xorg2;
+
+architecture dataflow of xorg2 is
+begin
+
+  o_F <= i_A xor i_B;
+
+end dataflow;
---
+++ proj/test/tb_dffg.do
@@ -0,0 +1,11 @@
+set target "tb_dffg"
+set file "${target}.vhd"
+
+#This line should be in every .do file!
+vcom -2008 -work work ../src/*.vhd
+
+vcom -2008 -work work $file
+
+vsim -voptargs=+acc $target
+add wave -position insertpoint \ ../$target/*
+run 1700
