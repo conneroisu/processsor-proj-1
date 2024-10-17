75578dea3c3afb5f91abbdfaa52078f6e95a943f
Conner Ohnesorge <connero@iastate.edu>
2024-10-17 08:38:36 -0500

    fix generic width instantiation of the program counter

--- proj/src/TopLevel/Fetch/program_counter.vhd
+++ proj/src/TopLevel/Fetch/program_counter.vhd
@@ -8,8 +8,8 @@ entity program_counter is
   port(
     i_CLK : in  std_logic;                       -- Clock input
     i_RST : in  std_logic;                       -- Reset input
-    i_D   : in  std_logic_vector(N downto 0);   -- Data value input
-    o_Q   : out std_logic_vector(N downto 0));  -- Data value output
+    i_D   : in  std_logic_vector(N-1 downto 0);   -- Data value input
+    o_Q   : out std_logic_vector(N-1 downto 0));  -- Data value output
 end program_counter;
 
 architecture structural of program_counter is
