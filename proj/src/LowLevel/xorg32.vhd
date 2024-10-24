5ed6ff58eb4087ad2d74e86e66dc3ab1e2f4634c
conneroisu <conneroisu@outlook.com>
2024-10-24 09:20:20 -0400

    added xorg32 component to the low level components (a 32 bit xor gate)

---
+++ proj/src/LowLevel/xorg32.vhd
@@ -0,0 +1,16 @@
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+entity xorg32 is
+    port (
+        i_A : in  std_logic_vector(31 downto 0);  -- input A
+        i_B : in  std_logic_vector(31 downto 0);  -- input B
+        o_F : out std_logic_vector(31 downto 0)   -- output F
+        );
+end xorg32;
+architecture dataflow of xorg32 is
+begin
+    G1 : for i in 0 to 31 generate
+        o_F(i) <= i_A(i) xor i_B(i);
+    end generate;
+end dataflow;
