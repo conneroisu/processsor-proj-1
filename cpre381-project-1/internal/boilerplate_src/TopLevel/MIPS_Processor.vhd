b5a56665f40e3f29a99c57921d68c81a61231f2a
conneroisu <conneroisu@outlook.com>
2024-10-24 10:02:10 -0400

    added better spacing to MIPS_Processor

--- cpre381-project-1/internal/boilerplate_src/TopLevel/MIPS_Processor.vhd
+++ cpre381-project-1/internal/boilerplate_src/TopLevel/MIPS_Processor.vhd
@@ -11,6 +11,7 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 library work;
 use work.MIPS_types.all;
+
 entity MIPS_Processor is
     generic(N : integer := DATA_WIDTH);
     port(iCLK      : in  std_logic;
@@ -20,6 +21,7 @@ entity MIPS_Processor is
          iInstExt  : in  std_logic_vector(N-1 downto 0);
          oALUOut   : out std_logic_vector(N-1 downto 0));  -- TODO: Hook this up to the output of the ALU. It is important for synthesis that you have this output that can effectively be impacted by all other components so they are not optimized away.
 end MIPS_Processor;
+
 architecture structure of MIPS_Processor is
     -- Required data memory signals
     signal s_DMemWr       : std_logic;  -- TODO: use this signal as the final active high data memory write enable signal
